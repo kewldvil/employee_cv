96d0e6d04a558ba827012b3aaceead5245e3e228
sopheak <nemsopheak@gmail.com>
2024-04-11 18:26:08 +0700

    second commit

---
+++ src/main/java/com/noc/employee_cv/contollers/DashboardController.java
@@ -0,0 +1,14 @@
+package com.noc.employee_cv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class DashboardController {
+    @GetMapping("/")
+    public String dashboard(){
+        return "dashboard";
+    }
+}
