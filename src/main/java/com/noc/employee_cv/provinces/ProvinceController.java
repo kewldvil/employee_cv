a2f3e0ed9396e89dfb63e99049e89e2cfc78558e
sopheak <nemsopheak@gmail.com>
2024-07-10 15:59:32 +0700

    fix province not loading after select default

--- src/main/java/com/noc/employee_cv/provinces/ProvinceController.java
+++ src/main/java/com/noc/employee_cv/provinces/ProvinceController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -24,22 +25,31 @@ public class ProvinceController {
     public ResponseEntity<List<ProvinceCity>> province() {
         List<ProvinceCity> provinceCities = provinceService.getAllProvinceCities();
         if (provinceCities.isEmpty()) {
-            return ResponseEntity.noContent().build(); // Return 204 No Content
+            return ResponseEntity.ok(Collections.emptyList());
         }
         return ResponseEntity.ok(provinceCities); // Return 200 OK with body
 
     }
     @GetMapping("/districts")
-    public ResponseEntity<List<District>> district(@RequestParam Integer province_id) {
+    public ResponseEntity<List<District>> district(@RequestParam(required = false, defaultValue = "0") Integer province_id) {
+        if (province_id == 0) {
+            return ResponseEntity.ok(Collections.emptyList()); // Return empty list if province_id is default value (0)
+        }
+
         List<District> districts = districtService.getAllDistrictById(province_id);
+
         if (districts.isEmpty()) {
             return ResponseEntity.noContent().build(); // Return 204 No Content
         }
-        return ResponseEntity.ok(districts); // Return 200 OK with body
 
+        return ResponseEntity.ok(districts); // Return 200 OK with body
     }
+
     @GetMapping("/communes")
-    public ResponseEntity<List<Commune>> commune(@RequestParam Integer district_id) {
+    public ResponseEntity<List<Commune>> commune(@RequestParam(required = false, defaultValue = "0") Integer district_id) {
+        if (district_id == 0) {
+            return ResponseEntity.ok(Collections.emptyList());
+        }
         List<Commune> communes = communeService.getAllCommuneById(district_id);
         if (communes.isEmpty()) {
             return ResponseEntity.noContent().build(); // Return 204 No Content
@@ -48,7 +58,10 @@ public class ProvinceController {
 
     }
     @GetMapping("/villages")
-    public ResponseEntity<List<Village>> village(@RequestParam Integer commune_id) {
+    public ResponseEntity<List<Village>> village(@RequestParam(required = false, defaultValue = "0") Integer commune_id) {
+        if (commune_id == 0) {
+            return ResponseEntity.ok(Collections.emptyList());
+        }
         List<Village> villages = villageService.getAllVillageById(commune_id);
         if (villages.isEmpty()) {
             return ResponseEntity.noContent().build(); // Return 204 No Content
